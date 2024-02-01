ef93224a17003396030b727537f1e065aeac94c4
danishkodemonkey <humberjini@hotmail.com>
2024-02-01 16:09:35 +0100

    Create javascript functions to color grid squares
    I created 2 functions for this, "addOnHover" adds a event listener
    to each generated div square, that executes the changeColor function
    on mouseover
    
    changeColor does exactly what it suggests, changes the color of the target
    element to black.

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+let screen = document.querySelector(".sketch-screen")
+
 function makeGrid(size = 16) {
-  let screen = document.querySelector(".sketch-screen")
   for (let i = 0; i < size; i++) {
     let column = document.createElement("div")
     column.classList.add("column")
-    row.classList.add("square")
+    column.classList.add("square")
     for (let j = 1; j <= size; j++) {
       let row = document.createElement("div")
       row.classList.add("row")
       row.classList.add("square")
-      row.style.border = "2px solid black"
+      row.style.border = "1px solid rgba(0,0,0,0.100)"
       column.appendChild(row)
     }
     screen.appendChild(column)
   }
+  addOnHover()
 }
-
-makeGrid()
+function addOnHover() {
+  const squares = screen.querySelectorAll("div.square")
+  squares.forEach((div) => {
+    div.addEventListener("mouseover", changeColor)
+  })
+}
+function changeColor(e) {
+  e.target.style.backgroundColor = "black"
+}
+makeGrid(100)
